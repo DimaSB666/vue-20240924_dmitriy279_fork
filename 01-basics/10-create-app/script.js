e4a323b844bd6e2d42b11e5044351ae75addb018
SB <bugsbunnu2336@gmail.com>
2024-09-30 23:04:16 +0300

    task create app is done

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,30 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'App',
+
+  setup() {
+    const localDate = new Date().toISOString()
+
+    function formatAsLocalDate() {
+      return new Date().toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+    }
+
+    return {
+      formatAsLocalDate,
+    }
+  },
+
+  template: `
+    <div>
+        Сегодня {{ formatAsLocalDate() }}
+    </div>
+  `,
+})
+
+const app = createApp(App)
+const vm = app.mount('#app')
